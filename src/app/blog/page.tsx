461545c4eb6c305c47d010898562f4e2b32cbea6
Rafael Matos <rafael-matos@outlook.com>
2026-01-06 22:07:17 -0300

    chore: add seo in blog page list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
+import type { Metadata } from "next";
 import { BlogList } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Dicas e estratégias para impulsionar seu negócio",
+    url: "https://siteblog-rm.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt-BR",
+    type: "website",
+    images: [
+      {
+        url: "https://siteblog-rm.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
